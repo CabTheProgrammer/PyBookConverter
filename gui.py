977a8acd9133b470792ce39f56376a41c51b47f0
CabTheProgrammer <55765253+CabTheProgrammer@users.noreply.github.com>
2020-06-13 13:55:27 -0500

    GUI before I switch over to grid instead of pack

--- gui.py
+++ gui.py
@@ -1,30 +1,42 @@
 import tkinter
 from tkinter import filedialog
 from PIL import ImageTk,Image
-import sys
-
-#import ebookmaker
+import ebookmaker
 
 
 #Initial stuff
-name="Ebook Compiler 0.1"
+name="Ebook Compiler 0.2"
 
+radio=0
 window=tkinter.Tk()
 window.title(name)
-window.geometry("400x400")
+window.geometry("500x600")
+window.minsize(width=300,height=300)
 
 
 
 #Functions
+
+def hide():
+    label4.pack_forget()
+    e12.pack_forget()
+
+def reveal():
+    print("This reveal the options for volume")
+    label4.pack()
+    e12.pack()
+
 def filech():
-    print("This is suppose to select the users directory")
+    print("This is supposed to select the users directory")
     filename=filedialog.askdirectory()
     print(filename)
-    filelabel.config(text=str(filename))
+    if filename != '':
+        filelabel.config(text=str(filename))
+        numlabel.config(text="Number of Chapters" + str(ebookmaker.dcount(filename)))
 
     
 def comp():
-    print("This is suppose to compile an ebook, based on file directory")
+    print("This is supposed to compile an ebook, based on file directory")
     Author=e11.get()
     Title=e1.get()
     print("Name of Ebook: "+Title)
@@ -44,17 +56,31 @@ def getCover():
 #Buttons and labels and entries
 bgcolor="grey"
 fgcolor="purple"
-label=tkinter.Label(window,text=name).pack(expand="yes",fill="both")
+label=tkinter.Label(window, text=name).pack(expand="yes",fill="both")
 bt=tkinter.Button(window,text="Choose Folder Directory",command=filech,bg=bgcolor,fg=fgcolor).pack()
 
 filelabel=tkinter.Label(window)
 filelabel.pack()
+numlabel=tkinter.Label(window)
+numlabel.pack()
+
+
+label4=tkinter.Label(window,text="Size of Chapter") #These two should only appear if the radio button for volumes are selected
+e12=tkinter.Entry(window,width=25)  #This is for the entry of the chapter size
+label4.pack()
+e12.pack()
+
 
 
 picbutton=tkinter.Button(window,text="Add Book Cover",bg=bgcolor,fg=fgcolor,command=getCover)
 picbutton.pack()
 piclabel=tkinter.Label(window)
 piclabel.pack()
+
+b1=tkinter.Radiobutton(window,text="Omnibus",variable=radio,value=1, command=hide)
+b2=tkinter.Radiobutton(window,text="Volume",variable=radio,value=2,command=reveal)
+b2.pack()
+b1.pack()
 
 
 label2=tkinter.Label(window,text="Enter name of ebook").pack(expand="yes",fill="both")
